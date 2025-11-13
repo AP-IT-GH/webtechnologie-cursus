56dcb94e9915d63e5e9d42b78809b47714fdf922
sam.vanbattel <sam.vanbattel@ap.be>
2025-11-13 07:51:48 +0000

    GITBOOK-93: No subject

--- .gitbook/assets/oplossingen/labo-12/oefening-14/js/script.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-let text = "Dit is een test. Deze tekst kan lang worden";
-let marge = 8;
-
-console.log(`
-/${"-".repeat(text.length + marge)}\\
-|${" ".repeat(marge / 2)}${text}${" ".repeat(marge / 2)}|
-\\${"-".repeat(text.length + marge)}/
-`);
