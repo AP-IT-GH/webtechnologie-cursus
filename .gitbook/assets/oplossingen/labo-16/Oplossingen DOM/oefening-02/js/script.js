ef3bcf1065e67584c78c475524d12b446b219a73
PhilippeSchraepen <schraepen.philippe@gmail.com>
2025-11-21 12:26:00 +0100

    updated oefeningen

--- .gitbook/assets/oplossingen/labo-16/Oplossingen DOM/oefening-02/js/script.js
+++
@@ -1,8 +0,0 @@
-"use strict"
-
-const myButton = document.querySelector('#myAction');
-
-myButton.addEventListener('click', function (e) {
-  const myP = document.querySelector('#myText');
-  myP.innerHTML = "Er werd op mij geklikt.";
-});
--- .gitbook/assets/oplossingen/labo-16/Oplossingen events/oefening-03/js/script.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-
-const newSource = "assets/images/nieuwe-afbeelding.png";
-document.querySelector("#myImage").src = newSource;
